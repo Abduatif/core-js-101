81d93c0198157bd793f999892820d3343f48fadc
Abdulatif <abdulatifa801@gmail.com>
2024-10-28 20:53:19 +0500

    feat:add basic files

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -584,8 +584,11 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const mid = Math.floor(arr.length / 2);
+  return arr.length % 2 === 0
+    ? arr.slice(mid).concat(arr.slice(0, mid))
+    : arr.slice(mid + 1).concat(arr[mid], arr.slice(0, mid));
 }
 
 
