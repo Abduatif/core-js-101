1e7df97cc9a45f6f9c0534986e6907972e0ac666
Abdulatif <abdulatifa801@gmail.com>
2024-10-30 19:18:12 +0500

    feat:add basic files

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -238,7 +238,7 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum( arr ) {
+function getMovingSum(arr) {
   return arr.reduce((acc, curr) => {
     const sum = (acc.length ? acc[acc.length - 1] : 0) + curr;
     acc.push(sum);
@@ -257,8 +257,8 @@ function getMovingSum( arr ) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((_, index) => index % 2 === 1);
 }
 
 
