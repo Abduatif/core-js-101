335b09f9033d3434f1c0c6a03dd3e713099f02ba
Abdulatif <abdulatifa801@gmail.com>
2024-10-31 11:14:03 +0500

    feat:add basic files

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -74,8 +74,15 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const deff = endDate.getTime() - startDate.getTime();
+
+  const hour = Math.floor(deff / 1000 / 60 / 60).toString().padStart(2, '0');
+  const minut = Math.floor((deff / 1000 / 60) % 60).toString().padStart(2, '0');
+  const second = Math.floor((deff / 1000) % 60).toString().padStart(2, '0');
+  const ms = (deff % 1000).toString().padStart(3, '0');
+
+  return `${hour}:${minut}:${second}.${ms}`;
 }
 
 
